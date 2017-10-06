e0c6cc7d1ff83655897a5e289cbd8885395a973f
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-10-06 16:24:38 -0500

    changes to URL for connection to Heroku

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Sort from './sorting.js'
 import ListForm from './listForm.js'
 import List from './list.js';
-const URL = 'mongodb://heroku_wbt1qf0m:9lpliscqqjdvqsmin1vavo376e@ds111895.mlab.com:11895/heroku_wbt1qf0m';
+const URL = 'https://finalprojectbackend.herokuapp.com/';
 
 //this is the main component. Information is sent to and from the API here. Logic dealing with state lives here.
 
@@ -51,10 +51,7 @@ class ToDoList extends Component {
       this.setState({
         toDoList: data,
         finalList: data,
-        isComplete: false,
-        description: '',
-        personResponsible: '',
-        dueDate: ''
+
       });
     })
   }
